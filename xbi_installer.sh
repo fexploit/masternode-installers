fde0bd1a9860ea9fe34c3ca05363ca41959e9c1d
Faruk <fexploit@users.noreply.github.com>
2019-06-08 15:16:28 +0300

    fixed unzip error

--- xbi_installer.sh
+++ xbi_installer.sh
@@ -66,7 +66,9 @@ function download_node() {
   cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
-  tar xvzf $COIN_ZIP -C $COIN_PATH >/dev/null 2>&1
+  unzip $COIN_ZIP
+  mv xbid $COIN_PATH
+  mv xbi-cli $COIN_PATH
   compile_error
   cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
